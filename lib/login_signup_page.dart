ef5084ea7185927bedf6d3a0a1413eb47e751912
Mikko Keinänen <mikko.keinanen@granlund.fi>
2019-01-30 20:19:59 +0200

    stateful widget

--- lib/login_signup_page.dart
+++ lib/login_signup_page.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-class LoginSignUpPage extends StatelessWidget {
+class LoginSignUpPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _LoginSignUpPageState();
+}
+
+class _LoginSignUpPageState extends State<LoginSignUpPage> {
 
   @override
   Widget build(BuildContext context) {
