3a9c36fab0cfd26a49f61727a9c43992aaee5e2d
Mikko Keinänen <mikko.keinanen@granlund.fi>
2019-01-30 20:15:32 +0200

    initial commit

---
+++ lib/login_signup_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class LoginSignUpPage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter login demo"),
+      ),
+      body: Container(
+        child: Text("Hello world"),
+      ),
+    );
+  }
+}
