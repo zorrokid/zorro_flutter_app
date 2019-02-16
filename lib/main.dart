e86c9cdf712af40c57aefb2083cabc8d94057cd6
Mikko Keinänen <mikko.keinanen@granlund.fi>
2019-02-16 19:21:57 +0200

    trying out scoped model

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutterapp/services/authentication.dart';
 import 'package:flutterapp/pages/root_page.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:flutterapp/models/flutterapp_model.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  final appModel = FlutterAppModel();
+  runApp(
+    ScopedModel<FlutterAppModel>(
+      model: appModel,
+      child: MyApp(),
+    )
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
